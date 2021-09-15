2610c9497cc087e48156991efc2d97c5f88f25fb
Alien Coder <paidcodecamp@gmail.com>
2021-09-15 11:35:35 +0530

    Fixed repeate event issue.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 const App = () => {
   const [lastBlockFetched, setLastBlockFetched] = useState({});
   const url = "https://mainnet.infura.io/v3/98444e7274b244e2a93abbee91210037";
   const provider = new ethers.providers.JsonRpcProvider(url);
-  try {
-    provider.on("block", (blockNumber) => {
-      getBlock(blockNumber);
-    });
-    const getBlock = async (blockNumber) => {
-      const block = await provider.getBlockWithTransactions(blockNumber);
-      setLastBlockFetched(block);
-      console.log(block);
-    };
-  } catch (e) {
-    console.log(e);
-  }
+  useEffect(() => {
+    try {
+      provider.on("block", (blockNumber) => {
+        if (Object.keys(lastBlockFetched)[0] !== blockNumber)
+          getBlock(blockNumber);
+      });
+      const getBlock = async (blockNumber) => {
+        const block = await provider.getBlockWithTransactions(blockNumber);
+        setLastBlockFetched(block);
+        console.log(block);
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
 
   return <div>Hello</div>;
 };
